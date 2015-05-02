e65c9c71047112087d931619f030447d21ce9773
Xavier Garceau-Aranda <xavier.garceau-aranda@polymtl.ca>
2015-05-02 11:44:23 -0400

    updated files

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,10 @@
+set fish_greeting:
+
 alias l="clear ; ls -l"
+
+set fish_color_command:00ffd7
+set fish_color_cwd:5fff00
+set fish_color_escape:cyan
+set fish_color_operator:cyan
+set fish_color_param:00afff\x1ecyan
+set fish_color_quote:d7ff00
--- i3/output_keymaps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# is CA
-if (setxkbmap -print | grep xkb_symbols | grep us); then
-    echo CA
-# is US
-else
-    echo US
-fi
